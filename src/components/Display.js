5ef8febf29d7cb3b7c93c4e628186ccbc130065d
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-01-09 15:10:41 -0800

    created input filed to enter topics and change the api address depending on that entry. the Display component displays the news in the result for that topic

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-const Display = () => {
+const Display = (props) => {
+  console.log("props in the Display", props);
   return (
     <div>
       <h1>Display</h1>
+
+      {props.news.map((article) => (
+        <div key={article.url}>
+          <h5>{article.title} </h5>
+          <p>section: {article.section}</p>
+        </div>
+      ))}
     </div>
   );
 };

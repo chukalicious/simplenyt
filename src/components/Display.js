379ed106d971a793bca9fc3b0d3902c66aee5f2d
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-01-09 17:22:44 -0800

    fixed the loading in the display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,18 +1,31 @@
 import React from "react";
+import { Article, NewsPage, TitleDiv } from "../styles";
 
 const Display = (props) => {
   console.log("props in the Display", props);
   return (
-    <div>
-      <h1>Display</h1>
-
-      {props.news.map((article) => (
-        <div key={article.url}>
-          <h5>{article.title} </h5>
-          <p>section: {article.section}</p>
+    <NewsPage>
+      <TitleDiv>
+        <h1>Top Stories</h1>
+        <h2>from the NYT</h2>
+      </TitleDiv>
+      {props.loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {props.news.map((article) => (
+            <Article key={article.url}>
+              <h3>{article.title} </h3>
+              <p>section: {article.section}</p>
+              <p>{article.abstract}</p>
+              <p>{article.byline}</p>
+              {/* name to change format on date */}
+              <p>published on: {article.published_date}</p>
+            </Article>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </NewsPage>
   );
 };
 
---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const NewsPage = styled.div`
+  background-color: yellow;
+  display: flex;
+  flex-direction: column;
+`;
+export const TitleDiv = styled.div`
+  color: blue;
+`;
+export const Article = styled.div`
+  color: red;
+  width: 40%;
+  border: solid red 1px;
+  border-radius: 3px;
+  flex-wrap: wrap;
+`;
